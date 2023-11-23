0c2f8b4c149158e3cbfc392aa49540923f17cc57
Ozzy Gonzalez <ozzy.gonzalez@onesixsolutions.com>
2023-11-22 18:00:56 -0800

    added ef core and scaffolded db

---
+++ BookStore.API/Data/BookStoreContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.API.Data;
+
+public partial class BookStoreContext : DbContext
+{
+    public BookStoreContext()
+    {
+    }
+
+    public BookStoreContext(DbContextOptions<BookStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost,1433;database=bookstore;user=sa;password=0zzyG0nz;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.Property(e => e.FirstName)
+                .IsRequired()
+                .HasMaxLength(60);
+            entity.Property(e => e.LastName)
+                .IsRequired()
+                .HasMaxLength(60);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasIndex(e => e.Isbn, "UQ__Books__9271CED090C0A4BA").IsUnique();
+
+            entity.Property(e => e.Image).IsRequired();
+            entity.Property(e => e.Isbn)
+                .IsRequired()
+                .HasMaxLength(50);
+            entity.Property(e => e.Summary)
+                .IsRequired()
+                .HasMaxLength(60);
+            entity.Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(30);
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books).HasForeignKey(d => d.AuthorId);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("BookStoreAppDbConnection");
+// builder.Services.AddDbContext<BookStoreContext>(options => options.UseSqlServer());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
